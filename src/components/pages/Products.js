11aa83300adfe1bc2773a42d466d3d8fb83ed0ea
Sumanth Kota <sumanthkota684@gmail.com>
2023-06-14 14:55:13 -0500

    New code

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -1,6 +1,134 @@
 import React from 'react';
-import '../../App.css';
+import '../../Products.css';
+import { useState } from 'react'
 
-export default function Products() {
-  return <h1 className='products'>Candidate Information</h1>;
-}
+const InformationForm = () => {
+  const [gender, setGender] = useState('');
+  const [citizenship, setCitizenship] = useState('');
+  const [legalRestrictions, setLegalRestrictions] = useState('');
+  const [graduatingClass, setGraduatingClass] = useState('');
+  const [collegeAttending, setCollegeAttending] = useState('');
+  const [gpa, setGPA] = useState('');
+  const [desiredPosition, setDesiredPosition] = useState('');
+
+  const handleGenderChange = (e) => {
+    setGender(e.target.value);
+  };
+
+  const handleCitizenshipChange = (e) => {
+    setCitizenship(e.target.value);
+  };
+
+  const handleLegalRestrictionsChange = (e) => {
+    setLegalRestrictions(e.target.value);
+  };
+
+  const handleGraduatingClassChange = (e) => {
+    setGraduatingClass(e.target.value);
+  };
+
+  const handleCollegeAttendingChange = (e) => {
+    setCollegeAttending(e.target.value);
+  };
+
+  const handleGPAChange = (e) => {
+    setGPA(e.target.value);
+  };
+
+  const handleDesiredPositionChange = (e) => {
+    setDesiredPosition(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform form submission or data handling here
+  };
+
+  return (
+    <html>
+    <head>
+    <link rel="stylesheet" type = "text/css" href="./style.css" />
+    </head>
+    <body>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Candidate First Name:
+        <input type="text" required />
+      </label>
+
+      <label>
+        Gender:
+        <select value={gender} onChange={handleGenderChange} required>
+          <option value="">Select an option</option>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+          <option value="Other">Other</option>
+        </select>
+      </label>
+
+      <label>
+        Citizenship/Work status:
+        <select value={citizenship} onChange={handleCitizenshipChange} required>
+          <option value="">Select an option</option>
+          <option value="Citizen">Citizen</option>
+          <option value="Permanent Resident">Permanent Resident</option>
+          <option value="Work Visa">Work Visa</option>
+          {/* Add more options as needed */}
+        </select>
+      </label>
+
+      <label>
+        Any other legal restrictions:
+        <select value={legalRestrictions} onChange={handleLegalRestrictionsChange} required>
+          <option value="">Select an option</option>
+          <option value="Yes">Yes</option>
+          <option value="No">No</option>
+        </select>
+      </label>
+
+      <label>
+        Last Name:
+        <input type="text" required />
+      </label>
+
+      <label>
+        Graduating class:
+        <select value={graduatingClass} onChange={handleGraduatingClassChange} required>
+          <option value="">Select an option</option>
+          <option value="2022">2022</option>
+          <option value="2023">2023</option>
+          <option value="2024">2024</option>
+          {/* Add more options as needed */}
+        </select>
+      </label>
+
+      <label>
+        Resume Attachment:
+        <input type="file" required />
+      </label>
+
+      <label>
+        College Attending:
+        <input type="text" required />
+      </label>
+
+      <label>
+        GPA:
+        <input type="number" step="0.01" value={gpa} onChange={handleGPAChange} required />
+      </label>
+
+      <label>
+        Desired Position:
+        <input type="text" value={desiredPosition} onChange={handleDesiredPositionChange} required />
+      </label>
+
+      <button type="submit">Submit</button>
+    </form>
+    </body>
+    </html>
+  );
+};
+
+export default InformationForm;
+
+
--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,6 +1,51 @@
 import React from 'react';
-import '../../App.css';
+import { useState } from 'react';
+import '../../SignUp.css';
+const SignupForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-export default function SignUp() {
-  return <h1 className='sign-up'>Sign up now</h1>;
-}
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform form submission or data handling here
+  };
+
+  return (
+    <div className="signup-form">
+      <h2>Create an Account</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">State Farm Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">State Farm Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignupForm;
